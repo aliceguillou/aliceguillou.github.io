e22c565a293729a18e5659b2e66ccc1bac70b45e
aliceguillou <guilloualice902@gmail.com>
2024-12-18 00:56:48 -0500

    Add files via upload

---
+++ Finalproject1/Final/project/training.js
@@ -0,0 +1,14 @@
+      function playAudio(command)
+
+  {
+    const audio = new audio(`Sitaudacity.mp3`); audio.play().catch((error) =>
+
+      { console.error("Error playing audio:", error);});
+  }
+
+
+
+
+
+
+
